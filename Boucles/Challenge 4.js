5080d2ee3626d6a1bb17349427e18b33ad1368f3
essanhaji bilal <bilalsenhaji88@gmail.com>
2026-09-19 12:25:39 +0200

    Initial commit

---
+++ Boucles/Challenge 4.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync')();
+
+
+let n = Number(prompt("entrer le nombre :"));
+
+for(let i = 0;i<n ;i++){
+    
+    console.log(2*i+1);//dert hadi hintache hiya form dyal nombre impair 
+    
+}
